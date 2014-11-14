563d4b34533585d25b6b3795183107f5afb515b2
Robert B Colton <robertbcolton@gmail.com>
2014-11-13 23:30:56 -0500

    Update GLSLKeywords.java

--- org/lateralgm/joshedit/lexers/GLSLKeywords.java
+++ org/lateralgm/joshedit/lexers/GLSLKeywords.java
@@ -55,7 +55,7 @@ public final class GLSLKeywords {
     try (InputStream is = GLSLKeywords.class.getResourceAsStream("glslkeywords.properties")) { //$NON-NLS-1$
       p.load(is);
     } catch (IOException e) {
-      e.printStackTrace();
+      Runner.showDefaultExceptionHandler(e);
     }
     String[] s = p.getProperty("CONSTRUCTS").split("\\s+"); //$NON-NLS-1$ //$NON-NLS-2$
     CONSTRUCTS = new Construct[s.length];
@@ -92,7 +92,7 @@ public final class GLSLKeywords {
         }
       }
     } catch (IOException e) {
-      e.printStackTrace();
+      Runner.showDefaultExceptionHandler(e);
     }
 
     FUNCTIONS = funcList.toArray(new Function[0]);
